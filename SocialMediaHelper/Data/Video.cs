a34631925f863510c280c735214b5ecf069e436d
mark <mark@penmark.com>
2020-04-14 16:19:12 -0400

    initial commit

---
+++ SocialMediaHelper/Data/Video.cs
@@ -0,0 +1,136 @@
+﻿namespace SocialMediaHelper.Data
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Video
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag")]
+        public string Etag { get; set; }
+
+        [JsonProperty("nextPageToken")]
+        public string NextPageToken { get; set; }
+
+        [JsonProperty("regionCode")]
+        public string RegionCode { get; set; }
+
+        [JsonProperty("pageInfo")]
+        public PageInfo PageInfo { get; set; }
+
+        [JsonProperty("items")]
+        public List<Item> Items { get; set; }
+    }
+
+    public partial class Item
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("etag")]
+        public string Etag { get; set; }
+
+        [JsonProperty("id")]
+        public Id Id { get; set; }
+
+        [JsonProperty("snippet")]
+        public Snippet Snippet { get; set; }
+
+        public bool Checked { get; set; }
+    }
+
+    public partial class Id
+    {
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+
+        [JsonProperty("videoId")]
+        public string VideoId { get; set; }
+    }
+
+    public partial class Snippet
+    {
+        [JsonProperty("publishedAt")]
+        public DateTimeOffset PublishedAt { get; set; }
+
+        [JsonProperty("channelId")]
+        public string ChannelId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("thumbnails")]
+        public Thumbnails Thumbnails { get; set; }
+
+        [JsonProperty("channelTitle")]
+        public string ChannelTitle { get; set; }
+
+        [JsonProperty("liveBroadcastContent")]
+        public string LiveBroadcastContent { get; set; }
+    }
+
+    public partial class Thumbnails
+    {
+        [JsonProperty("default")]
+        public Default Default { get; set; }
+
+        [JsonProperty("medium")]
+        public Default Medium { get; set; }
+
+        [JsonProperty("high")]
+        public Default High { get; set; }
+    }
+
+    public partial class Default
+    {
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+
+        [JsonProperty("height")]
+        public long Height { get; set; }
+    }
+
+    public partial class PageInfo
+    {
+        [JsonProperty("totalResults")]
+        public long TotalResults { get; set; }
+
+        [JsonProperty("resultsPerPage")]
+        public long ResultsPerPage { get; set; }
+    }
+
+    public partial class Video
+    {
+        public static Video FromJson(string json) => JsonConvert.DeserializeObject<Video>(json, SocialMediaHelper.Data.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Video self) => JsonConvert.SerializeObject(self, SocialMediaHelper.Data.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ SocialMediaHelper/Data/YouTubeInputs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaHelper.Data
+{
+    public class YouTubeInputs
+    {
+        public string part { get; set; }
+        public int maxResults { get; set; }
+        public string q { get; set; }
+        public string order { get; set; }
+        //public string order { get; set; }
+        public string baseUrl { get; set; }
+        public string key { get; set; }
+    }
+}
